3b2b0477ba47d565be4525748a1c00d70322f559
Jason Fan <jsonfan@gmail.com>
2015-11-24 15:52:50 -0800

    orders controller and model barebones

--- application/controllers/orders.php
+++ application/controllers/orders.php
@@ -1,33 +1,34 @@
 <?php
 
 
-class Orders extends CI_Controller {
+class Orders extends CI_Controller
+{
 
-  public function __construct()
-	{
-		parent::__construct();
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('Order');
+        $this->load->library('form_validation');
 
-		$this->load->library('form_validation');
+    }
 
-	}
+    public function checkout_view()
+    {
+        //loads checkout view with session[cart] data
+    }
 
-  public function checkout_view(){
-    //loads checkout view with cart data
-  }
+    public function checkout()
+    {
+        //processes POST data on SUBMIT PURCHASE BUTTON
+        //ADD ORDER
+        //RETRIEVES INSERTED ID
+        //REDIRECTS TO CONFIRMATION/$id
+    }
 
-  public function checkout(){
-    //processes POST data on SUBMIT PURCHASE BUTTON
-    //RETRIEVES INSERTED ID
-    //REDIRECTS TO CONFIRMATION/$id
-  }
-
-  public function confirmation($order_id){
-
-  }
-
-
-
-
-
+    public function confirmation($order_id)
+    {
+        //get_order_by_id($order_id)
+        //load view (confirmation, order_details)
+    }
 
 }
