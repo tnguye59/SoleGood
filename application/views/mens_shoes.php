1791fb66e363806a2fe14c6fb5599df3d0ae2575
Jason Fan <jsonfan@gmail.com>
2015-11-23 16:48:22 -0800

    jVIEWS

---
+++ application/views/mens_shoes.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+   <head>
+      <meta charset="utf-8">
+      <title>MENS SHOES</title>
+      <style>
+      body {
+    padding-top: 65px;
+}
+
+      </style>
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
+   </head>
+   <body>
+      <div class='container'>
+      <nav class="navbar navbar-inverse navbar-fixed-top">
+         <div class="container">
+            <div class="navbar-header">
+               <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+               <span class="sr-only">Toggle navigation</span>
+               <span class="icon-bar"></span>
+               <span class="icon-bar"></span>
+               <span class="icon-bar"></span>
+               </button>
+               <a class="navbar-brand" href="#">SHOEOHOESHOHESO</a>
+            </div>
+            <div id="navbar" class="navbar-collapse collapse">
+               <form class="navbar-form navbar-right" action='' method='post'>
+                  <div class="form-group">
+                     <input type="text" placeholder="Email" class="form-control">
+                  </div>
+                  <div class="form-group">
+                     <input type="password" placeholder="Password" class="form-control">
+                  </div>
+                  <button type="submit" class="btn btn-success">Sign in</button>
+               </form>
+            </div>
+            <!--/.navbar-collapse -->
+         </div>
+      </nav>
+      <!-- Sidebar -->
+        <div id="sidebar-wrapper">
+          <ul>
+            <li>hoop</li>
+            <li>running</li>
+            <li>casual</li>
+            <li>dress</li>
+          </ul>
+        </div>
+
+        <!-- /#sidebar-wrapper -->
+        <div class="content">
+          PRODUCTS
+          <ul>
+            <li>PRODUCT</li>
+            <li>PRODUCT</li>
+            <li>PRODUCT</li>
+            <li>PRODUCT</li>
+            <li>PRODUCT</li>
+            <li>PRODUCT</li>
+            <li>PRODUCT</li>
+            <li>PRODUCT</li>
+          </ul>
+
+        </div>
+      </div>
+   </body>
+</html>
