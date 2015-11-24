ebc39b8944d2df93c28a47595c325ce6ea2cbe31
Jason Fan <jsonfan@gmail.com>
2015-11-23 17:54:50 -0800

    ERIC

---
+++ application/views/admin_new_product.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Add New Product</title>
+  </head>
+  <body>
+    <div class="container">
+      <h1>Add a new product</h1>
+        <form class="" action="index.html" method="post">
+          <label>Name <input type="text" name="name"></label>
+          <label>Order Description <input type="text" name="description"></label>
+          <label>Shipping Address <input type="text" name="shipping"></label>
+          <label>Billing Addres <input type="text" name="billing"></label>
+          <label>Order Total <input type="text" name="total"></label>
+          <input type="submit" name="new_product" value="Create">
+        </form>
+    </div>
+  </body>
+</html>
--- application/views/admin_view_orders.php
+++ application/views/admin_view_orders.php
@@ -1,1 +1,17 @@
-admin_view_orders.php
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>View Product</title>
+  </head>
+  <body>
+    <div class="container">
+      <h1>Product ID</h1>
+      <p> Name: product name</p>
+      <p>Description: order description</p>
+      <p>Shipping Address</p>
+      <p>Billing Address Address</p>
+      <p>Order Total</p>
+    </div>
+  </body>
+</html>
