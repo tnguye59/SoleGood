f5430a5fb0a5144d2b6fe4848a9d50a0eb5f327a
Eric Chou <ericchou2010@gmail.com>
2015-11-25 10:49:09 -0800

    this better work

--- application/views/confirmation.php
+++ application/views/confirmation.php
@@ -2,22 +2,81 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Confirmation Page</title>
-	<meta charset = "utf-8">
-		<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-		<link rel="stylesheet" type="text/css" href="/assets/css/confirmation.css">
+    <title>Confirmation Page</title>
+    <meta charset = "utf-8">
+        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="/assets/css/confirmation.css">
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Sole Shoes Dashboard</title>
+
+
+    <!-- Bootstrap Core CSS -->
+    <link href="/assets/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="/assets/css/modern-business.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="/assets/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
 </head>
 
 <body>
-	<div id = "wrapper">
-		<h3>Confirmation</h3>
-		<p>Thank you for order, (echo name)! Your order details are below.</p>
-		<div id = "orderinfo">TROLOLOL</div>
-		<p id = "shippinginfo">Shipping Address: (echo shipping address)</p>
-		<a href = "#">Contact Us |</a>
-		<a href = "#">Return Home |</a>
-		<a href="#">Logout</a>
-	</div>
+     <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="/welcome/shoes">Sole Good!</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <a href="about.html">Register/Login</a>
+                    </li>
+                    <li>
+                        <a href="services.html">Help</a>
+                    </li>
+                    <li>
+                        <a href="/welcome/checkout">Cart</a>
+                    </li>
+                    <li>
+                        <a href="/welcome/about">About</a>
+                    </li>
+                    <li>
+                        <a href="/welcome/dashboard">About</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+    <div>
+        <a href="/products/product"><button>Continue Shopping!</button></a>
+    </div>
+    <div id = "wrapper">
+        <h3>Confirmation</h3>
+        <p>Thank you for your order, (echo name)! Your order details are below.</p>
+        <div id = "orderinfo">TROLOLOL</div>
+        <p id = "shippinginfo">Shipping Address: (echo shipping address)</p>
+        <a href = "#">Contact Us |</a>
+        <a href = "#">Return Home |</a>
+        <a href="#">Logout</a>
+    </div>
 
 </body>
 </html>
