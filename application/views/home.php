2c2115629d4104a5f2e8eb693325622446576d5a
Tommy <tnguye59@gmail.com>
2015-11-25 13:23:05 -0800

    update drop down menu

--- application/views/home.php
+++ application/views/home.php
@@ -41,9 +41,23 @@
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav navbar-right">
-                  <li>
-                    <a href="/welcome/shoes">SHOP NOW</a>
-                  </li>
+                   <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">SHOP NOW <b class="caret"></b></a>
+                        <ul class="dropdown-menu">
+                            <li>
+                                <a href="portfolio-1-col.html">Men</a>
+                            </li>
+                            <li>
+                                <a href="portfolio-2-col.html">Women</a>
+                            </li>
+                            <li>
+                                <a href="portfolio-3-col.html">Boys</a>
+                            </li>
+                            <li>
+                                <a href="portfolio-4-col.html">Girls</a>
+                            </li>
+                        </ul>
+                    </li>
                     <li>
 <?php
                       if($this->session->userdata('userInfo')){
