cf15234f980d186435d0727fb18b9a91c988e229
Jason Fan <jsonfan@gmail.com>
2015-11-27 15:48:05 -0800

    updated about page

--- application/views/about.php
+++ application/views/about.php
@@ -9,7 +9,22 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Sole Shoes about page</title>
+    <title>About Us</title>
+    <style media="screen">
+      @font-face{
+        font-family: bop;
+        src: url('/assets/font-awesome/fonts/bop.ttf');
+      }
+      .logo{
+        color: white;
+        font-size: 2em;
+        font-family: bop;
+      }
+      #map {
+       height: 400px;
+       width: 400px;
+     }
+    </style>
 
     <!-- Bootstrap Core CSS -->
     <link href="/assets/css/bootstrap.min.css" rel="stylesheet">
@@ -31,40 +46,76 @@
 
 <body>
 
-    <!-- Navigation -->
-    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-        <div class="container">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="index.html">Sole Good!</a>
-            </div>
-            <!-- Collect the nav links, forms, and other content for toggling -->
-               <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav navbar-right">
-                    <li>
-                        <a href="about.html">Register/Login</a>
-                    </li>
-                    <li>
-                        <a href="services.html">Help</a>
-                    </li>
-                    <li>
-                        <a href="contact.html">Cart</a>
-                    </li>
-                    <li>
-                        <a href="/welcome/about">About</a>
-                    </li>
-                </ul>
-            </div>
-            <!-- /.navbar-collapse -->
-        </div>
-        <!-- /.container -->
-    </nav>
+  <!-- Navigation -->
+  <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div class="container">
+          <!-- Brand and toggle get grouped for better mobile display -->
+          <div class="navbar-header">
+              <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                  <span class="sr-only">Toggle navigation</span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+              </button>
+              <a class='logo' class="navbar-brand" href="/">Sole Good!</a>
+          </div>
+          <!-- Collect the nav links, forms, and other content for toggling -->
+          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+              <ul class="nav navbar-nav navbar-right">
+                 <li class="dropdown">
+                      <a href="#" class="dropdown-toggle" data-toggle="dropdown">SHOP NOW <b class="caret"></b></a>
+                      <ul class="dropdown-menu">
+                        <li>
+                            <a href="/products/main_product_page">All Products</a>
+                        </li>
+                          <li>
+                              <a href="/products/mens_products">Men</a>
+                          </li>
+                          <li>
+                              <a href="/products/womens_products">Women</a>
+                          </li>
+                          <li>
+                              <a href="/products/boys_products">Boys</a>
+                          </li>
+                          <li>
+                              <a href="/products/girls_products">Girls</a>
+                          </li>
+                      </ul>
+                  </li>
+                  <li>
+<?php
+                    if($this->session->userdata('userInfo')){
+?>
+
+                      <a href="/users/dashboard">DASHBOARD</a>
+<?php                 } else {
+
+?>
+                      <a href="/users">Register/Login</a>
+<?php                   }
+?>
+                  </li>
+                  <li>
+                      <a href="/welcome/cart">Cart</a>
+                  </li>
+                  <li>
+                      <a href="/welcome/about">About</a>
+                  </li>
+<?php
+                  if($this->session->userdata('userInfo')){
+?>
+                  <li>
+                    <a href="/users/logout">Logout</a>
+                  </li>
+<?php                 }
+?>
+              </ul>
+          </div>
+          <!-- /.navbar-collapse -->
+      </div>
+      <!-- /.container -->
+  </nav>
+
 
     <!-- Page Content -->
     <div class="container">
@@ -72,9 +123,12 @@
         <!-- Page Heading/Breadcrumbs -->
         <div class="row">
             <div class="col-lg-12">
-                <h1 class="page-header">About
-                    <small>Subheading</small>
+                <h1 class="page-header">About Us
+
                 </h1>
+                <p>
+                  <small>Questions? Want to say hello?</small>
+                </p>
                 <ol class="breadcrumb">
                     <li><a href="index.html">Home</a>
                     </li>
@@ -87,16 +141,37 @@
         <!-- Intro Content -->
         <div class="row">
             <div class="col-md-6">
-                <img class="img-responsive" src="http://placehold.it/750x450" alt="">
+              <div id="map"></div>
+         <script>
+
+      var map;
+      var myLatLng = {lat: 34.098601, lng: -118.0991417};
+      function initMap() {
+       map = new google.maps.Map(document.getElementById('map'), {
+         center: myLatLng,
+         zoom: 15
+       });
+
+       var marker = new google.maps.Marker({
+        position: myLatLng,
+        map: map,
+        title: 'We are here!'
+  });
+      }
+
+         </script>
+         <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyARVP4t7S29h65zpgU6iBjiKbJj4HCXB1o&callback=initMap"
+             async defer></script>
             </div>
             <div class="col-md-6">
-                <h2>About Modern Business</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed voluptate nihil eum consectetur similique? Consectetur, quod, incidunt, harum nisi dolores delectus reprehenderit voluptatem perferendis dicta dolorem non blanditiis ex fugiat.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe, magni, aperiam vitae illum voluptatum aut sequi impedit non velit ab ea pariatur sint quidem corporis eveniet. Odit, temporibus reprehenderit dolorum!</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et, consequuntur, modi mollitia corporis ipsa voluptate corrupti eum ratione ex ea praesentium quibusdam? Aut, in eum facere corrupti necessitatibus perspiciatis quis?</p>
+                <h2>About Sole Good!</h2>
+              <p>
+                We are blah blah blah blah blah blah blah blah blah
+              </p>
             </div>
         </div>
         <!-- /.row -->
+
 
         <!-- Team Members -->
         <div class="row">
@@ -247,7 +322,6 @@
         <!-- /.row -->
 
         <hr>
-        <h4><a href="/welcome/contact">Contact us</a></h4>
 
         <!-- Footer -->
         <footer>
