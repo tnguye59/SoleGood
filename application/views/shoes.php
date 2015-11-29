ccd864c082d61681d23f8be9bb16a0a98474f9ea
Tommy <tnguye59@gmail.com>
2015-11-28 17:18:26 -0800

    debugg

--- application/views/shoes.php
+++ application/views/shoes.php
@@ -241,40 +241,13 @@
         <hr>
 
         <!-- Pagination -->
-        <div class="row text-center">
-            <div class="col-lg-12">
-                <ul class="pagination">
-                    <li>
-                        <a href="#">&laquo;</a>
-                    </li>
-                    <li class="active">
-                        <a href="#">1</a>
-                    </li>
-                    <li>
-                        <a href="#">2</a>
-                    </li>
-                    <li>
-                        <a href="#">3</a>
-                    </li>
-                    <li>
-                        <a href="#">4</a>
-                    </li>
-                    <li>
-                        <a href="#">5</a>
-                    </li>
-                    <li>
-                        <a href="#">&raquo;</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
         <!-- /.row -->
 
         <!-- Footer -->
         <footer>
             <div class="row">
                 <div class="col-lg-12">
-                    <p>Copyright &copy; Sole Good 2014</p>
+                    <p>Copyright &copy; Sole Good 2015</p>
                 </div>
             </div>
         </footer>
